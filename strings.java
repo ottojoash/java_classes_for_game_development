0a6e4bd3b726197e096cbaa444acfc5e525538af
ottojoash <louisjoshbricks@gmail.com>
2022-11-10 06:52:22 -0500

    strings

---
+++ strings.java
@@ -0,0 +1,12 @@
+public class Apllication{
+    public static void main(String[] args){
+        int aNumber = 9;
+        String text = "hi boo";
+        String blank = "";
+        String moreText = "there";
+        String hithere = text + blank + moreText;
+        System.out.println(text);
+        System.out.println(text+ blank + moreText);
+        System.out.println("hi boo"+ ""+"there");
+    }
+}
